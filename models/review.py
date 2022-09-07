29b77c384656f66ab92669de3a65a47ea02fce67
rasheey97_alt <rasheey97@yahoo.com>
2022-09-06 19:47:55 -0700

    task o

---
+++ models/review.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Class Review """
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Review class that inherits BaseModel """
+    place_id = ""
+    user_id = ""
+    text = ""#!/usr/bin/python3
+""" Class Review """
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Review class that inherits BaseModel """
+    place_id = ""
+    user_id = ""
+    text = ""
